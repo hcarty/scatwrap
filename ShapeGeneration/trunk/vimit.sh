73da0ae345c52cbf03abe85fcd2a630ce9d1d24f
zerook <zerook@eb4173e2-d0fc-0310-ad7e-817b8e051b15>
2006-07-19 15:52:05 +0000

    r142@laptop44:  hcarty | 2006-07-19 11:50:24 -0400  * Small change - open the database-building files too.
    git-svn-id: http://vcs.0ok.org/scatwrap@85 eb4173e2-d0fc-0310-ad7e-817b8e051b15

--- ShapeGeneration/trunk/vimit.sh
+++ ShapeGeneration/trunk/vimit.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gvim -p test_scatwrap.pl test_parameters.yaml ToDo.otl ScatWrap/DDSCAT.pm ScatWrap/IO.pm ScatWrap/Math.pm ScatWrap/Shape.pm
+gvim -p test_scatwrap.pl test_parameters.yaml ToDo.otl ScatWrap/DDSCAT.pm ScatWrap/IO.pm ScatWrap/Math.pm ScatWrap/Shape.pm create_tables.pl database_schema.yaml
 
